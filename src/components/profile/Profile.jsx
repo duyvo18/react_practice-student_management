162ca4d83eb4f6b74e65a9f5284323ab6015e640
vkduy19 <vkduy19@apcs.fitus.edu.vn>
2022-06-12 16:24:46 +0700

    init profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../common/Header";
 
 const Profile = () => {
-
+    return (
+        <div className="w-screen h-screen">
+            <Header from="profile" />
+        </div>
+    )
 }
 
 export default Profile
