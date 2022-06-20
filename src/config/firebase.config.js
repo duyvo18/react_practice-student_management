c100c65d74bde8c530910627ad62b852b01607bb
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-06-20 10:44:38 +0700

    remove local var for firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -3,11 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
-
-console.log(getApps().length)
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-console.log(getApps().length)
+const app = getApps().length > 0 ? getApp() : initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
 
 const firestore = getFirestore(app);
 const storage = getStorage(app);
