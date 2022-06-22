f2b1cabf8be41f19fc98ec9d243b73e448f5bd93
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-06-22 10:09:07 +0700

    update responsive not found

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -4,13 +4,13 @@ const NotFound = () => {
     return (
         <div className="min-w-screen min-h-screen bg-secondary flex flex-col justify-center items-center">
             <div className="flex flex-col flex-auto justify-center items-center">
-                <div className="text-6xl font-bold text-accent">
+                <div className="text-3xl sm:text-6xl font-bold text-accent">
                     404
                 </div>
-                <div className="text-4xl font-semibold mt-4">
+                <div className="text-2xl sm:text-4xl font-semibold mt-4">
                     Page Not Found
                 </div>
-                <div className="text-2xl mt-10">
+                <div className="text-xl sm:text-2xl mt-10">
                     Got lost? Return to our <a className="underline hover:text-gray-500" href="/">main page</a>.
                 </div>
             </div>
