8b5ec5a5f93aecbe5a5185ef57fade356c813e08
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-06-22 10:09:19 +0700

    update responsive server error

--- src/components/common/ServerError.jsx
+++ src/components/common/ServerError.jsx
@@ -17,20 +17,20 @@ const ServerError = () => {
 
     return (
         <div className="min-w-screen min-h-screen bg-secondary flex flex-col justify-center items-center">
-            <div className="flex flex-col flex-auto justify-center items-center text-center">
-                <div className="text-6xl font-bold text-accent">
+            <div className="flex flex-col flex-auto justify-center items-center text-center px-3">
+                <div className="text-3xl sm:text-6xl font-bold text-accent">
                     OOPS...
                 </div>
-                <div className="text-4xl font-semibold mt-4">
+                <div className="text-2xl sm:text-4xl font-semibold mt-4">
                     Unexpected error from server
                 </div>
-                <div className="text-xl mt-6">
+                <div className="text-md sm:text-xl mt-6">
                     Name: <span className="italic">"{name}"</span>
                 </div>
-                <div className="text-xl mt-1">
+                <div className="text-md sm:text-xl mt-1">
                     Message: <span className="italic">"{message}"</span>
                 </div>
-                <div className="text-2xl mt-10">
+                <div className="text-lg sm:text-2xl mt-10">
                     Unfortunately, we don't know how the deal with this problem yet.<br />
                     For now, please <a className="underline hover:text-gray-500" href="/">click here</a> to return to our main page.
                 </div>
